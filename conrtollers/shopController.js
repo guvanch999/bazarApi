2e8adbf3926a2dc1cef267f8a5a6b35658f4a2eb
MAIN <guvanch.d.1999@gmail.com>
2022-05-20 17:38:29 +0500

    31.01.2022

--- conrtollers/shopController.js
+++ conrtollers/shopController.js
@@ -1,101 +1,136 @@
-const pool=require('../db/db');
-const queries=require('../sqlqueries/shopQueries');
-const sender=require('../utils/sendRespond');
-const functionsUSE=require('../db/bigRequersts');
-const promiseFunctions=require('../utils/promisiFunctions');
+const pool = require('../db/db');
+const queries = require('../sqlqueries/shopQueries');
+const sender = require('../utils/sendRespond');
+const functionsUSE = require('../db/bigRequersts');
+const promiseFunctions = require('../utils/promisiFunctions');
+const {queryExequterWithThenBlock} = require("../utils/promisiFunctions");
+const {calculateRating} = require("../utils/useFullFunctions");
 
-var getShopMiniById=async (req,res)=>{
-      var shop_id=req.params.id;
-      if(shop_id===undefined){
-          return sender.sendRespondInvalidParams(res,req.lang);
-      }
-      await pool.query(queries.SHOPDETAILSMINI({id:shop_id}),(err,rows)=>{
-          if(err){
-              console.log(err);
-              return sender.sendRespondInternalSErr(res,req.lang);
-          }
-          return sender.sendSuccess(res,rows);
-      })
-}
-var getShopFullById=async (req,res)=>{
-    var shop_id=req.params.id;
-    if(shop_id===undefined){
-        return sender.sendRespondInvalidParams(res,req.lang);
+var getShopMiniById = async (req, res) => {
+    var shop_id = req.params.id;
+    if (shop_id === undefined) {
+        return sender.sendRespondInvalidParams(res, req.lang);
     }
-    await pool.query(queries.SHOPDETAILSFULL({id:shop_id}),(err,rows)=>{
-        if(err){
+    await pool.query(queries.SHOPDETAILSMINI({id: shop_id}), (err, rows) => {
+        if (err) {
             console.log(err);
-            return sender.sendRespondInternalSErr(res,req.lang);
+            return sender.sendRespondInternalSErr(res, req.lang);
         }
-        return sender.sendSuccess(res,rows);
+        return sender.sendSuccess(res, rows);
     })
 }
-var getShopAds=async (req,res)=>{
-    var shop_id=req.params.id;
-    if(shop_id===undefined){
-        return sender.sendRespondInvalidParams(res,req.lang);
+var getShopFullById = async (req, res) => {
+    var shop_id = req.params.id;
+    if (shop_id === undefined) {
+        return sender.sendRespondInvalidParams(res, req.lang);
     }
-    await pool.query(queries.SHOPADSBYID({id:shop_id}),(err,rows)=>{
-        if(err){
+    let data = await getFullShopDetail(shop_id)
+    if(!data){
+        return sender.sendRespondInternalSErr(res,req.lang)
+    }
+    return sender.sendSuccess(res,data)
+}
+var getShopAds = async (req, res) => {
+    var shop_id = req.params.id;
+    if (shop_id === undefined) {
+        return sender.sendRespondInvalidParams(res, req.lang);
+    }
+    await pool.query(queries.SHOPADSBYID({id: shop_id}), (err, rows) => {
+        if (err) {
             console.log(err);
-            return sender.sendRespondInternalSErr(res,req.lang);
+            return sender.sendRespondInternalSErr(res, req.lang);
         }
-        return sender.sendSuccess(res,rows);
+        return sender.sendSuccess(res, rows);
     })
 }
-var getShopBanners=async (req,res)=>{
-    var shop_id=req.params.id;
-    if(shop_id===undefined){
-        return sender.sendRespondInvalidParams(res,req.lang);
+var getShopBanners = async (req, res) => {
+    var shop_id = req.params.id;
+    if (shop_id === undefined) {
+        return sender.sendRespondInvalidParams(res, req.lang);
     }
-    await pool.query(queries.SHOPBANNERS({id:shop_id}),(err,rows)=>{
-        if(err){
+    await pool.query(queries.SHOPBANNERS({id: shop_id}), (err, rows) => {
+        if (err) {
             console.log(err);
-            return sender.sendRespondInternalSErr(res,req.lang);
+            return sender.sendRespondInternalSErr(res, req.lang);
         }
-        return sender.sendSuccess(res,rows);
+        return sender.sendSuccess(res, rows);
     })
 }
-var getShopCatalogs=async (req,res)=>{
-    var shop_id=req.params.id;
-    if(shop_id===undefined){
-        return sender.sendRespondInvalidParams(res,req.lang);
+var getShopCatalogs = async (req, res) => {
+    var shop_id = req.params.id;
+    if (shop_id === undefined) {
+        return sender.sendRespondInvalidParams(res, req.lang);
     }
-    await pool.query(queries.SHOPKATALOGS({id:shop_id}),(err,rows)=>{
-        if(err){
+    await pool.query(queries.SHOPKATALOGS({id: shop_id}), (err, rows) => {
+        if (err) {
             console.log(err);
-            return sender.sendRespondInternalSErr(res,req.lang);
+            return sender.sendRespondInternalSErr(res, req.lang);
         }
-        return sender.sendSuccess(res,rows);
+        return sender.sendSuccess(res, rows);
     })
 }
 
 
-
-var getShopsWithCckg=async (req,res)=>{
-    let catalog_id=req.params.catalog_id||null;
-    if(catalog_id==null){
-        return sender.sendRespondInvalidParams(res,req.lang);
+var getShopsWithCckg = async (req, res) => {
+    let catalog_id = req.params.catalog_id || null;
+    if (catalog_id == null) {
+        return sender.sendRespondInvalidParams(res, req.lang);
     }
-    let page=req.url_queries.page||1;
-    let resultRows=await functionsUSE.getShopsWithChecking(catalog_id,req.user.user_id,page);
-    if(resultRows){
-        return sender.sendSuccess(res,resultRows);
+    let page = req.url_queries.page || 1;
+    let resultRows = await functionsUSE.getShopsWithChecking(catalog_id, req.user.user_id, page);
+    if (resultRows) {
+        return sender.sendSuccess(res, resultRows);
     } else {
-        return sender.sendRespondInternalSErr(res,req.lang);
+        return sender.sendRespondInternalSErr(res, req.lang);
     }
 }
-let getBolumShops=async (req,res)=>{
-    let _bolum_id=req.params.bolum_id;
-    if(!_bolum_id){
-        return sender.sendRespondInvalidParams(res,req.lang);
+let getBolumShops = async (req, res) => {
+    let _bolum_id = req.params.bolum_id;
+    if (!_bolum_id) {
+        return sender.sendRespondInvalidParams(res, req.lang);
     }
-    await promiseFunctions.queryExequterWithThenBlock(queries.BOLUM_SHOPS({bolum_id:_bolum_id})).then(rows=>{
-        return sender.sendSuccess(res,rows);
-    }).catch(err=>{
+    await promiseFunctions.queryExequterWithThenBlock(queries.BOLUM_SHOPS({bolum_id: _bolum_id})).then(rows => {
+        return sender.sendSuccess(res, rows);
+    }).catch(err => {
         console.log(err);
-        return sender.sendRespondInternalSErr(res,req.lang);
+        return sender.sendRespondInternalSErr(res, req.lang);
     })
+
+}
+
+async function getFullShopDetail(shop_id) {
+    return await queryExequterWithThenBlock(queries.SHOPDETAILSFULL({id: shop_id}))
+        .then(rows => {
+            if (rows.length) {
+                return rows[0]
+            } else {
+                return false
+            }
+        }).then(async data => {
+            if (!data) {
+                return false
+            } else {
+                let listBanners = await queryExequterWithThenBlock(queries.SHOPBANNERS, [shop_id])
+                let listVideos = await queryExequterWithThenBlock(queries.SHOP_VIDEOS, [shop_id])
+                let follow = await queryExequterWithThenBlock(queries.FOLLOW_COUNT,[shop_id]);
+                let product = await queryExequterWithThenBlock(queries.COUNTOFPRODUCTS({shop_id}))
+                let katalogs = await queryExequterWithThenBlock(queries.GET_SHOP_KATALOGS + shop_id);
+                let ratings = await queryExequterWithThenBlock(queries.GET_SHOP_RATING, [shop_id])
+                let rating = calculateRating(ratings)
+                rating['totalRatingCount'] = ratings.length;
+                data['rating'] = rating;
+                return Object.assign(data, {
+                    followCount: follow[0].total,
+                    productCount: product[0].total,
+                    listBanners,
+                    listVideos,
+                    katalogs
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+            return false
+        })
 
 }
 
--- sqlqueries/shopQueries.js
+++ sqlqueries/shopQueries.js
@@ -2,9 +2,9 @@ module.exports = {
     SHOPDETAILSMINI:(params)=>`select * from shop where id=${params.id}`,
     SHOPDETAILSFULL:(params)=>`select * from shop where id=${params.id}`,
     SHOPADSBYID:(params)=>`select * from ads_fromshops where shop_id=${params.id}`,
-    SHOPBANNERS:(params)=>`select * from banner where shop_id=${params.id}`,
-
-
+    SHOPBANNERS:`select * from shop_banner where shop_id=?`,
+    SHOP_VIDEOS:"select * from shop_videos where shop_id=?",
+    FOLLOW_COUNT:"select count(*) as total from follow where shop_id=?",
     SHOPKATALOGS:(params)=>`select k.* from katalog as k inner join shop_katalog as sk on k.id=sk.katalog_id where sk.shop_id=${params.id}`,
 
 
@@ -18,8 +18,9 @@ module.exports = {
     COUNTOFFOLLOWERS:(params)=>`select count(*) as total from follow where shop_id=${params.shop_id}`,
     COUNTOFPRODUCTS:(params)=>`select count(*) as total from product where shop_id=${params.shop_id}`,
     //done
+    GET_SHOP_KATALOGS:"select k.* from katalog k inner join shop_katalog sc on sc.katalog_id=k.id and sc.shop_id=",
 
-
+    GET_SHOP_RATING:"select * from rating_shops where shop_id=?",
     BOLUM_SHOPS:(params)=>`select s.* from shop_bolum sb inner join shop s on s.id=sb.shop_id where sb.bolum_id=${params.bolum_id}`,
     //shop detailda yyly hasaplatmaly
     //shop detailda hasaplatmaly zatlar kop
