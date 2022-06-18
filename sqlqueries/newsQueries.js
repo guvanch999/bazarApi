25983df50a9c2fb7ea1c898e97b17d87f58b69d5
MAIN <guvanch.d.1999@gmail.com>
2022-06-18 12:33:01 +0500

    31.01.2022

--- sqlqueries/newsQueries.js
+++ sqlqueries/newsQueries.js
@@ -14,8 +14,8 @@ module.exports = {
     },
     GET_NEWS_IMAGES:`select photo from tazelik_photo where tazelik_id=?`,
     GET_NEWS_DETAIL:'select * from tazelik where id=?',
-    GET_SHOP_DETAIL:"select shop_name,vip,(select count(*) from product where shop_id=?) as productCount from shop where id=?",
-    GET_SERVICE_DETAIL:"select service_shops_name as shop_name,vip,(select count(*) from service_product where service_id=?) as productCount from service_shops where id=?",
+    GET_SHOP_DETAIL:"select shop_name,photo,vip,(select count(*) from product where shop_id=?) as productCount from shop where id=?",
+    GET_SERVICE_DETAIL:"select service_shops_name as shop_name,photo,vip,(select count(*) from service_product where service_id=?) as productCount from service_shops where id=?",
 
     GET_USER_FOLLOWS:"select * from follow where user_id=?",
     GET_SHOP_FOR_FOLLOW:'select shop_name,vip,photo from shop where id=?',
